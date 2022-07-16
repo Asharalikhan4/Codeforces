f13ea08bec14b7e093ca5c62282342ba61339671
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2022-07-16 09:56:42 +0530

    Create 1A - Theatre square.cpp

---
+++ 1A - Theatre square.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int main(){
+	double n, m, a;
+	cin>>n>>m>>a;
+	cout<<(long long)ceil(n/a)*(long long)ceil(m/a)<<endl;
+	return 0;
+}
